deff40096831f6e808f1d6172843876edf2f2ef4
Olawale Akinro <akinbolawa90@gmail.com>
2022-08-14 10:48:43 -0700

    Initial Commit

---
+++ Display Firebase Data/Display_Firebase_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Display_Firebase_DataApp.swift
+//  Display Firebase Data
+//
+//  Created by swift-rn-flutter on 8/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Display_Firebase_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
